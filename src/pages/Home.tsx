b443a92f231bec92b2373cc2a9fc81e1121ea7a1
Giulex84 <giulianom.gm@gmail.com>
2025-12-18 08:07:01 +0100

    feat: add router, layout, home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return <h1>Pactpi</h1
+
+cat << 'EOF' > src/App.tsx
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import Home from "./pages/Home";
+
+export default function
+
+cat << 'EOF' >> src/App.tsx
+function App
+cat << 'EOF' > src/pages/Home.tsx
+export default function Home() {
+  return <h1>Pactpi</h1>;
+}
