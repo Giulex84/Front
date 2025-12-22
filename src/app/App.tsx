87c14a06de9e2b3cbdc0c9b41cf6014f4b190b54
Giulex84 <giulianom.gm@gmail.com>
2025-12-22 01:30:17 +0100

    Change import paths to use relative references
    Updated import paths for pages to use relative paths.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
-import CreatePact from "./pages/CreatePact";
-import PactDetail from "./pages/PactDetail";
-import Profile from "./pages/Profile";
+
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Dashboard from "../pages/Dashboard";
+import CreatePact from "../pages/CreatePact";
+import PactDetail from "../pages/PactDetail";
+import Profile from "../pages/Profile";
 
 const App: React.FC = () => {
   const path = window.location.pathname;
