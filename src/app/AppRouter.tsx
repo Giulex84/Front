1598c2af5f67b54becd7eaf2c4c2f47ae7fe19e2
Giulex84 <giulianom.gm@gmail.com>
2025-12-23 21:03:15 +0100

    Update AppRouter.tsx

--- src/app/AppRouter.tsx
+++ src/app/AppRouter.tsx
@@ -1,18 +1,8 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "../pages/Home";
-import About from "../pages/About";
-import Agreements from "../pages/Agreements";
-import Privacy from "../pages/Privacy";
-import Terms from "../pages/Terms";
-
 export default function AppRouter() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/agreements" element={<Agreements />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/privacy" element={<Privacy />} />
-      <Route path="/terms" element={<Terms />} />
-    </Routes>
+    <div style={{ padding: 20 }}>
+      <h1>PactPi</h1>
+      <p>Logged in with Pi Network ✅</p>
+    </div>
   );
 }
