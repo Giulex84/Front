1ce227d88a0cc34c0418d301b4c468632c26cd95
Giulex84 <giulianom.gm@gmail.com>
2025-12-24 00:44:11 +0100

    Update pi.d.ts

--- src/types/pi.d.ts
+++ src/types/pi.d.ts
@@ -13,6 +13,20 @@ declare global {
           username: string;
         };
       }>;
+
+      createPayment: (
+        paymentData: {
+          amount: number;
+          memo: string;
+          metadata?: Record<string, any>;
+        },
+        callbacks: {
+          onReadyForServerApproval?: (paymentId: string) => void;
+          onReadyForServerCompletion?: (paymentId: string) => void;
+          onCancel?: () => void;
+          onError?: (error: any) => void;
+        }
+      ) => Promise<any>;
     };
   }
 }
