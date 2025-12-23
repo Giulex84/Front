13d2bec6b34914c3b7444ad580a9e26ce7ff05c1
Giulex84 <giulianom.gm@gmail.com>
2025-12-23 22:22:03 +0100

    Update AppRouter.tsx

--- src/app/AppRouter.tsx
+++ src/app/AppRouter.tsx
@@ -1,8 +1,12 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
 import Home from "../pages/Home";
 import Dashboard from "../pages/Dashboard";
+import CreatePact from "../pages/CreatePact";
+import Profile from "../pages/Profile";
 import Privacy from "../pages/Privacy";
 import Terms from "../pages/Terms";
+
 import type { PiUser } from "./App";
 
 type AppRouterProps = {
@@ -13,7 +17,7 @@ export default function AppRouter({ user }: AppRouterProps) {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Public */}
+        {/* PUBLIC */}
         <Route
           path="/"
           element={user ? <Navigate to="/dashboard" /> : <Home />}
@@ -21,12 +25,23 @@ export default function AppRouter({ user }: AppRouterProps) {
         <Route path="/privacy" element={<Privacy />} />
         <Route path="/terms" element={<Terms />} />
 
-        {/* Protected */}
+        {/* PROTECTED */}
         <Route
           path="/dashboard"
           element={user ? <Dashboard /> : <Navigate to="/" />}
         />
 
+        <Route
+          path="/create"
+          element={user ? <CreatePact /> : <Navigate to="/" />}
+        />
+
+        <Route
+          path="/profile"
+          element={user ? <Profile /> : <Navigate to="/" />}
+        />
+
+        {/* FALLBACK */}
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
