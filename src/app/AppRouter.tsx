809b671c255bde0a0eb8ba7aad14d66d3188e53b
Giulex84 <giulianom.gm@gmail.com>
2025-12-23 21:38:18 +0100

    Update AppRouter.tsx

--- src/app/AppRouter.tsx
+++ src/app/AppRouter.tsx
@@ -1,21 +1,31 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "../pages/Home";
+import Dashboard from "../pages/Dashboard";
 import Privacy from "../pages/Privacy";
 import Terms from "../pages/Terms";
+import { PiUser } from "./App";
 
-export default function AppRouter() {
+type Props = {
+  user: PiUser | null;
+};
+
+export default function AppRouter({ user }: Props) {
   return (
     <BrowserRouter>
       <Routes>
-        {/* HOME DEFAULT */}
-        <Route path="/" element={<Home />} />
-
-        {/* LEGAL */}
+        {/* Pagine pubbliche */}
+        <Route path="/" element={user ? <Navigate to="/dashboard" /> : <Home />} />
         <Route path="/privacy" element={<Privacy />} />
         <Route path="/terms" element={<Terms />} />
 
-        {/* FALLBACK */}
-        <Route path="*" element={<Navigate to="/" replace />} />
+        {/* Pagine protette */}
+        <Route
+          path="/dashboard"
+          element={user ? <Dashboard /> : <Navigate to="/" />}
+        />
+
+        {/* Fallback */}
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
   );
