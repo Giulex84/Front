5415e31739cb9cb85c5d36424bbc2839aaab1e4b
Giulex84 <giulianom.gm@gmail.com>
2025-12-24 01:29:55 +0100

    Update pi.d.ts

--- src/types/pi.d.ts
+++ src/types/pi.d.ts
@@ -2,33 +2,31 @@ export {};
 
 declare global {
   interface Window {
-    Pi: PiSDK;
-  }
+    Pi: {
+      authenticate: (
+        scopes: string[],
+        onIncompletePaymentFound?: (payment: any) => void
+      ) => Promise<{
+        accessToken: string;
+        user: {
+          uid: string;
+          username: string;
+        };
+      }>;
 
-  interface PiSDK {
-    authenticate: (
-      scopes: string[],
-      onIncompletePaymentFound?: (payment: any) => void
-    ) => Promise<{
-      accessToken: string;
-      user: {
-        uid: string;
-        username: string;
-      };
-    }>;
-
-    createPayment: (
-      paymentData: {
-        amount: number;
-        memo: string;
-        metadata?: any;
-      },
-      callbacks: {
-        onReadyForServerApproval?: (paymentId: string) => void;
-        onReadyForServerCompletion?: (paymentId: string, txid: string) => void;
-        onCancel?: () => void;
-        onError?: (error: any) => void;
-      }
-    ) => void;
+      createPayment: (
+        paymentData: {
+          amount: number;
+          memo: string;
+          metadata?: any;
+        },
+        callbacks: {
+          onReadyForServerCompletion?: () => void;
+          onReadyForServerApproval?: () => void;
+          onCancel?: () => void;
+          onError?: (error: any) => void;
+        }
+      ) => void;
+    };
   }
 }
