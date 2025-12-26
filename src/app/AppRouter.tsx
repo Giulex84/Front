661e0e4199eb41134d9b4298b3da26c7f8ec55af
Giulex84 <giulianom.gm@gmail.com>
2025-12-26 20:39:10 +0100

    Update AppRouter.tsx

--- src/app/AppRouter.tsx
+++ src/app/AppRouter.tsx
@@ -1,12 +1,16 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "../pages/Home";
+// src/app/AppRouter.tsx
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import VerifyPi from "../pages/VerifyPi";
+import Home from "../pages/Home";
 
 export default function AppRouter() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/verify" element={<VerifyPi />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/verify" element={<VerifyPi />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
