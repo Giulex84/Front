c4746cf93be327b6ac51f97f371145531fba58a8
Giulex84 <giulianom.gm@gmail.com>
2025-12-26 23:52:53 +0100

    Update AppRouter.tsx

--- src/app/AppRouter.tsx
+++ src/app/AppRouter.tsx
@@ -1,36 +1,26 @@
 import { Routes, Route } from "react-router-dom";
 
 import Home from "../pages/Home";
-import About from "../pages/About";
 import Agreements from "../pages/Agreements";
-import Privacy from "../pages/Privacy";
-import Terms from "../pages/Terms";
-import VerifyPi from "../pages/VerifyPi";
-import Dashboard from "../pages/Dashboard";
 import CreatePact from "../pages/CreatePact";
 import PactDetail from "../pages/PactDetail";
-import Profile from "../pages/Profile";
-import Login from "../pages/Login";
+import VerifyPi from "../pages/VerifyPi";
 
 export default function AppRouter() {
   return (
     <Routes>
-      {/* Public */}
       <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/privacy" element={<Privacy />} />
-      <Route path="/terms" element={<Terms />} />
-      <Route path="/login" element={<Login />} />
 
-      {/* Core app */}
-      <Route path="/dashboard" element={<Dashboard />} />
+      {/* Agreements */}
       <Route path="/agreements" element={<Agreements />} />
-      <Route path="/agreements/new" element={<CreatePact />} />
-      <Route path="/agreements/:id" element={<PactDetail />} />
-      <Route path="/profile" element={<Profile />} />
+      <Route path="/agreements/create" element={<CreatePact />} />
+      <Route path="/pact/:id" element={<PactDetail />} />
 
-      {/* Pi */}
+      {/* Pi verification */}
       <Route path="/verify" element={<VerifyPi />} />
+
+      {/* Fallback */}
+      <Route path="*" element={<Home />} />
     </Routes>
   );
 }
