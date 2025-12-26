612083bffcd53407f8c30ef3c6a037c4ca386dc5
Giulex84 <giulianom.gm@gmail.com>
2025-12-26 18:49:58 +0100

    Update AppRouter.tsx

--- src/app/AppRouter.tsx
+++ src/app/AppRouter.tsx
@@ -1,13 +1,8 @@
-import type { PiUser } from "../services/piAuth";
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import VerifyPi from "../pages/VerifyPi";
 
-type Props = {
-  user: PiUser | null;
-};
-
-export default function AppRouter({ user }: Props) {
+export default function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
