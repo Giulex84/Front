7dcf6d74872af7ed9fe7410a9066ffbaceb417f3
Giulex84 <giulianom.gm@gmail.com>
2025-12-26 18:24:23 +0100

    Update pi.d.ts

--- src/types/pi.d.ts
+++ src/types/pi.d.ts
@@ -3,33 +3,19 @@ export {};
 declare global {
   interface Window {
     Pi: {
-      authenticate: (
-        scopes: string[],
-        onIncompletePaymentFound?: (payment: any) => void
-      ) => Promise<{
-        accessToken: string;
-        user: {
-          uid: string;
-          username: string;
-        };
-      }>;
-
       createPayment: (
-        paymentData: {
+        payment: {
           amount: number;
-          memo: string;
+          memo?: string;
           metadata?: any;
         },
         callbacks: {
-          onReadyForServerApproval?: (paymentId: string) => void | Promise<void>;
-          onReadyForServerCompletion?: (
-            paymentId: string,
-            txid?: string
-          ) => void;
+          onReadyForServerApproval: (paymentId: string) => void;
+          onReadyForServerCompletion: (paymentId: string, txid: string) => void;
           onCancel?: () => void;
           onError?: (error: any) => void;
         }
-      ) => void;
+      ) => Promise<void>;
     };
   }
 }
